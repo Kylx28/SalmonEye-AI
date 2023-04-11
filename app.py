856ccd15a33ddd0f26b059b15405a810d8fd2a8f
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2023-04-11 14:22:22 -0400

    added salmon stream and AI detection

--- app.py
+++ app.py
@@ -1,36 +1,38 @@
 from flask import Flask, render_template, Response
+from ultralytics import YOLO
 import cv2
 
+model = YOLO('static/detect_salmon.pt')
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)  # use 0 for web camera
+camera = cv2.VideoCapture('/home/swarnava/devel/train/salmon_run.mp4')  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
 def gen_frames():  # generate frame by frame from camera
     while True:
         # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
+        success, frame = camera.read() # read the camera frame
         if not success:
-            break
+            camera.set(cv2.CAP_PROP_POS_FRAMES, 0) # repeat video
+            continue
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            results = model.predict(source=frame)
+            annotated_frame = results[0].plot()
+            ret, buffer = cv2.imencode('.jpg', annotated_frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
 
 @app.route('/video_feed')
 def video_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 @app.route('/')
 def index():
     #"""Video streaming home page."""
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
