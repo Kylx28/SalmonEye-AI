d1c296fb905d75cbe269f09da03140ddd7d07104
Swarnava Ghosh <swarnavaghosh04@gmail.com>
2023-04-12 16:40:49 -0400

    removed yolov8 verbose printing, added salmon count printing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Response
+from datetime import datetime
 from ultralytics import YOLO
 import cv2
 
@@ -9,15 +10,28 @@ camera = cv2.VideoCapture('static/salmon_run.mp4')  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
+
 def gen_frames():  # generate frame by frame from camera
+    threshold = 25
+    counter = 0
+    salmon_count = 0
     while True:
         # Capture frame-by-frame
         success, frame = camera.read() # read the camera frame
         if not success:
             camera.set(cv2.CAP_PROP_POS_FRAMES, 0) # repeat video
         else:
-            results = model(frame)
+            results = model.predict(source=frame, classes=15, verbose=False)
             annotated_frame = results[0].plot()
+
+            salmon_in_frame = len(results[0].boxes)
+            counter += salmon_in_frame
+            if counter >= threshold:
+                now = datetime.now()
+                print("Salmon Detected:", now)
+                salmon_count += 1
+                counter = 0
+
             ret, buffer = cv2.imencode('.jpg', annotated_frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
